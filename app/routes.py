5af9ea6510d5ed197f1d14e6841264528b029bb5
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2015-04-16 16:31:06 -0400

    basic flask app functioning in virtualenv

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+# CS421 Project
+# Hareesh Nagaraj, Thomas Gill, Joe Soultanis
+# Routes file
+from flask import Flask, render_template
+ 
+app = Flask(__name__)      
+ 
+@app.route('/')
+def home():
+  return render_template('home.html')
+ 
+if __name__ == '__main__':
+  app.run(debug=True)

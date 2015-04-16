da2559d2b7457a6c505483555bfeab26cf8e3651
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2015-04-16 16:54:57 -0400

    db test query functioning in routes.py

--- app/routes.py
+++ app/routes.py
@@ -8,8 +8,12 @@ import psycopg2
 #THIS IS THE CONNECTION TO THE CS COMP DATABASE ASSUMING LOCAL PORT
 #PORT VALUE NEEDS TO BE CHANGED FOR DEPLOYMENT
 conn = psycopg2.connect(database="nagaraj_weather", user="nagaraj", password="nagaraj", host="localhost", port="63333")
+cur = conn.cursor() #used to perform ops on db
 
- 
+cur.execute("SELECT * FROM weather")
+for record in cur:
+  print record
+
 app = Flask(__name__)      
  
 @app.route('/')

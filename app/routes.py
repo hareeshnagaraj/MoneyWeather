d1c9212532951e9dbe6093c3b3b16bb47b66aca0
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2015-04-16 16:48:52 -0400

    successful connection to database via local port

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,20 @@
 # Hareesh Nagaraj, Thomas Gill, Joe Soultanis
 # Routes file
 from flask import Flask, render_template
+import psycopg2
+
+
+#THIS IS THE CONNECTION TO THE CS COMP DATABASE ASSUMING LOCAL PORT
+#PORT VALUE NEEDS TO BE CHANGED FOR DEPLOYMENT
+conn = psycopg2.connect(database="nagaraj_weather", user="nagaraj", password="nagaraj", host="localhost", port="63333")
+
  
 app = Flask(__name__)      
  
 @app.route('/')
 def home():
+  print("hi")
   return render_template('home.html')
- 
+
 if __name__ == '__main__':
   app.run(debug=True)

f3c02a736314a3a3575daeab06c3c41f30a7d1d3
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2015-04-16 17:45:41 -0400

    weather data being pushed into db

--- app/populate_weather.py
+++ app/populate_weather.py
@@ -5,12 +5,18 @@
 
 import psycopg2
 import urllib2
+import csv
+
+#NOTE: THESE NEED UPDATING BEFORE EACH NEW QUERY
+airport = "KEKO"
+ZIP = 89801
 
 directory = "data/weather/"
-airport = "KEKO"
+
 
 urlp1 = "http://www.wunderground.com/history/airport/" 
 urlp2 = "/3/2/CustomHistory.html?dayend=2&monthend=3&yearend=2015&req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo=&format=1"
+
 
 def getURL(a,b,c,d):
     return a + b + "/" + str(c) + d
@@ -24,14 +30,27 @@ def populateWeather():
         output.write(csv.read())
         output.close()
         year += 1
-    year = 2000
+
+
+#Weather CSV Notes
+def updateDB():
+    with open(directory + 'nevada2001.csv', 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            print row[0]
 
 
 def main():
     print("Hi, it's time to grab some weather data!")
     print("Make sure you have the DB connected and configured")
     print("as well as the appropriate airport code - any mistakes will result in a cluttered database")
-    populateWeather()
+    # year = 2001
+    # url = getURL(urlp1,airport,year,urlp2)
+    # csv = urllib2.urlopen(url)
+    # output = open(directory + "nevada"+str(year)+".csv",'wb')
+    # output.write(csv.read())
+    # output.close()
+    updateDB()
 
 
 

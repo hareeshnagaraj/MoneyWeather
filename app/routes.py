db56ee49bdfe99798895de34ba1fa1d9afedff7b
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2015-04-20 15:00:45 -0400

    developing gold page

--- app/routes.py
+++ app/routes.py
@@ -14,8 +14,13 @@ app = Flask(__name__)
  
 @app.route('/')
 def home():
-  print("hi")
+  print("home page rendering")
   return render_template('index.html')
+
+@app.route('/gold')
+def tool():
+  print("tool page rendering")
+  return render_template('gold.html')
 
 if __name__ == '__main__':
   app.run(debug=True)

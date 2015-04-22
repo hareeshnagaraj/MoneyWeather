fef514088511a6290b90fb34bfc5d17aadcae9a7
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2015-04-22 18:05:42 -0400

    generating UI progress - abstracted further

--- app/static/js/commodity.js
+++ app/static/js/commodity.js
@@ -4,32 +4,46 @@ Mostly in jQuery, just for simplicity
 
 */
 
-//Globals
-var from_month;
-var from_year;
-var to_month;
-var to_year;
+//Function to be called from the template
 
-$(document).ready(function() 
- {
-    $('ul#from_month li').click(function(e) 
-    { 
-        from_month = $(this).find("a").text();
-        $("#from_month_label").text(from_month)
-    });
-    $('ul#from_year li').click(function(e) 
-    { 
-        from_year = $(this).find("a").text();
-        $("#from_year_label").text(from_year)
-    });
-    $('ul#to_month li').click(function(e) 
-    { 
-        to_month = $(this).find("a").text();
-        $("#to_month_label").text(to_month)
-    });
-    $('ul#to_year li').click(function(e) 
-    { 
-        to_year = $(this).find("a").text();
-        $("#to_year_label").text(to_year)
-    });
- });
+function GenerateUI( commodity, zipList )
+    {
+        var from_month = "Month";
+        var from_year = "Year";
+        var to_month = "Month";
+        var to_year = "Year";
+
+        var zipCodes = zipList;
+        console.log(zipCodes)
+
+        $('ul#from_month li').click(function(e) 
+        { 
+            from_month = $(this).find("a").text();
+            $("#from_month_label").text(from_month)
+        });
+        $('ul#from_year li').click(function(e) 
+        { 
+            from_year = $(this).find("a").text();
+            $("#from_year_label").text(from_year)
+        });
+        $('ul#to_month li').click(function(e) 
+        { 
+            to_month = $(this).find("a").text();
+            $("#to_month_label").text(to_month)
+        });
+        $('ul#to_year li').click(function(e) 
+        { 
+            to_year = $(this).find("a").text();
+            $("#to_year_label").text(to_year)
+        });
+
+        //Makes the call to load in the appropriate weather data necessary
+        $("#generateButton").click(function(){
+            if(from_month == "Month" || from_year == "Year" || to_month == "Month" || to_year == "Year"){
+                window.alert("Please enter a valid time range");
+                return ;
+            }
+
+            console.log("Valid time range");
+        });
+ }
